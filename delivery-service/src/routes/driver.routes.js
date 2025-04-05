a9f84b7f4e7b2cfb0c8d874dba2ca7ed595dd8c7
ASDENUWAN <sachinthaakarawita@gmail.com>
2025-04-05 20:54:06 +0530

    created routes for createDriver

---
+++ delivery-service/src/routes/driver.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { createDriver } from "../controllers/driver.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
+
+const router = express.Router();
+
+// Create a new driver record
+router.post("/add", createDriver);
+
+export default router;
