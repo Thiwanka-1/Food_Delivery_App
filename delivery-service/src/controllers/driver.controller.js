404d5a529e4068e19eca93072aa53edf8419ca92
ASDENUWAN <sachinthaakarawita@gmail.com>
2025-04-06 20:15:06 +0530

    created getDistanceFromLatLonInKm

--- delivery-service/src/controllers/driver.controller.js
+++ delivery-service/src/controllers/driver.controller.js
@@ -29,6 +29,21 @@ export const getDriverById = async (req, res, next) => {
   }
 };
 
+const deg2rad = (deg) => deg * (Math.PI / 180);
+const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+  const R = 6371; // Earth's radius in km
+  const dLat = deg2rad(lat2 - lat1);
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+};
+
 export const getDriverByUserId = async (req, res) => {
   try {
     const driver = await Driver.findOne({ userId: req.params.userId });
